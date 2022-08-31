cd0626bb2c72288a6348c8f5eb65f6b96e1f475a
Edward Blanciak <blanciak.edward2@gmail.com>
2022-08-31 16:28:13 -0400

    hash map complete

---
+++ classes/hash_map.py
@@ -0,0 +1,73 @@
+class HashMap:
+    def __init__(self):
+        self.size = 64 # sets default size to a fixed length
+        self.map = [None] * self.size # where data is stored
+
+    ''' 
+    Calculates the index for the key
+    '''
+    def _get_hash(self, key):
+        hash = 0
+        for char in str(key):
+            hash += ord(char)
+        return hash % self.size
+
+    '''
+    Inserts an element into the hash
+    '''
+    def insert(self, key, value):
+        key_hash = self._get_hash(key)
+        key_value = [key, value]
+
+        # Key Doesn't exist? Insert new key/value pair
+        if self.map[key_hash] is None:
+            self.map[key_hash] = list([key_value])
+            return True
+        else: # Exists? Update
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    pair[1] = value
+                    return True
+            self.map[key_hash].append(key_value)
+            return True
+
+    '''
+    Returns key/value pair
+    '''
+    def get(self, key):
+        key_hash = self._get_hash(key)
+        if self.map[key_hash] is not None:
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    return pair[1]
+        return None
+
+    '''
+    Deletes a key/value pair
+    '''
+    def delete(self, key):
+        key_hash = self._get_hash(key)
+
+        if self.map[key_hash] is None:
+            return False
+        for i in range(0, len(self.map[key_hash])):
+            if self.map[key_hash][i][0] == key:
+                self.map[key_hash].pop(i)
+                return True
+        return False
+
+
+    '''
+    Returns the keys of the hashmap
+    '''
+    def keys(self):
+        arr = []
+        for i in range(0, len(self.map)):
+            if self.map[i]:
+                arr.append(self.map[i][0])
+        return arr
+
+    def print(self):
+        for item in self.map:
+            if item is not None:
+                print(str(item))
