094f73b45069f662b0e8c58998f08d647993faf1
Edward Blanciak <blanciak.edward2@gmail.com>
2022-09-05 18:33:33 -0400

    hub class mostly complete. needs more detail added to comments

--- classes/hub.py
+++ classes/hub.py
@@ -6,19 +6,56 @@ import csv
 class Hub:
     def __init__(self):
         self.storage = HashTable()
-        self.package_log = HashTable()
+        self.storage_size = 0
+        self.distances = []
+        self.addresses = []
+
         self.truck_1 = Truck(1)
         self.truck_2 = Truck(2)
+        self.truck_3 = Truck(3)
 
+
+    # Returns a package
     def get_package(self, id):
-        return self.storage.lookup(id)
+        return self.storage.lookup(str(id))
+
+    # Returns an 2-dimensional array of distances
+    def get_distances(self):
+        return self.distances
+
+    # Returns the distance between two locations
+    # takes strings (full address) and returns a float (distance)
+    def get_distance_between(self, full_street_address_1, full_street_address_2):
+        location_1 = self._get_address_matrix_number(full_street_address_1)
+        location_2 = self._get_address_matrix_number(full_street_address_2)
+        return float(self.distances[location_1][location_2])
+
+    # Returns the address number id (0-27) -- used for locating the address in the distance table
+    def _get_address_matrix_number(self, full_street_address):
+        for address in self.addresses:
+            if address[2] == full_street_address:
+                return int(address[0])
+
+
+    # Stores addresses
+    def add_addresses(self, file_path):
+        with open(file_path) as address_file:
+            address_data = csv.reader(address_file, delimiter=",", quotechar='"')
+            for address in address_data:
+                self.addresses.append(address)
+
+    # Stores distances
+    def add_distances(self, file_path):
+        with open(file_path) as distance_file:
+            distance_data = csv.reader(distance_file, delimiter=",", quotechar='"')
+            for distance in distance_data:
+                self.distances.append(distance)
 
     # Adds packages to storage
     def add_packages(self, file_path):
         # read the package data file
         with open(file_path) as package_file:
-            package_data = csv.reader(package_file, delimiter=",",
-                                      quotechar='"')
+            package_data = csv.reader(package_file, delimiter=",", quotechar='"')
             for package in package_data:
                 # create a new Package
                 id = package[0]
@@ -34,16 +71,19 @@ class Hub:
 
                 # Insert the new package
                 self.storage.insert(id, new_package)
+                self.storage_size+=1
 
-    # check truck package logs if any updates and update the master log
-    def update_package_log(self):
-        return
-
-    # Transfers packages to trucks from storage
-    def transfer_packages_to_truck(self, package_key, truck_number):
-        # add to truck by using Truck methods
-        self.storage.delete(package_key)
 
     # Prints all packages in storage
     def print_packages(self):
         self.storage.print()
+
+    # Prints all distances between addresses
+    def print_distances(self):
+        for distance in self.distances:
+            print(distance)
+
+    # Prints all address information
+    def print_addresses(self):
+        for address in self.addresses:
+            print(address)
