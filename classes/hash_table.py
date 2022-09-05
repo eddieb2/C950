a0be29fb4f70b5ed6632792db3a1beeba3a18f61
Edward Blanciak <blanciak.edward2@gmail.com>
2022-09-05 12:19:08 -0400

    changed class name

--- classes/hash_table.py
+++ classes/hash_table.py
@@ -1,4 +1,5 @@
-class HashMap:
+import itertools
+class HashTable:
     def __init__(self):
         self.size = 64 # sets default size to a fixed length
         self.map = [None] * self.size # where data is stored
@@ -8,12 +9,14 @@ class HashMap:
     '''
     def _get_hash(self, key):
         hash = 0
+
         for char in str(key):
             hash += ord(char)
+
         return hash % self.size
 
     '''
-    Inserts an element into the hash
+    Inserts an element into the hash O(1)
     '''
     def insert(self, key, value):
         key_hash = self._get_hash(key)
@@ -21,39 +24,47 @@ class HashMap:
 
         # Key Doesn't exist? Insert new key/value pair
         if self.map[key_hash] is None:
+
             self.map[key_hash] = list([key_value])
             return True
+
         else: # Exists? Update
             for pair in self.map[key_hash]:
+
                 if pair[0] == key:
                     pair[1] = value
                     return True
+
             self.map[key_hash].append(key_value)
             return True
 
     '''
     Returns key/value pair
     '''
-    def get(self, key):
+    def lookup(self, key):
         key_hash = self._get_hash(key)
+
         if self.map[key_hash] is not None:
             for pair in self.map[key_hash]:
                 if pair[0] == key:
                     return pair[1]
+
         return None
 
     '''
-    Deletes a key/value pair
+    Deletes a key/value pair O(1)
     '''
     def delete(self, key):
         key_hash = self._get_hash(key)
 
         if self.map[key_hash] is None:
             return False
+
         for i in range(0, len(self.map[key_hash])):
             if self.map[key_hash][i][0] == key:
                 self.map[key_hash].pop(i)
                 return True
+
         return False
 
 
@@ -67,7 +78,17 @@ class HashMap:
                 arr.append(self.map[i][0])
         return arr
 
+
     def print(self):
+        arr = []
         for item in self.map:
             if item is not None:
-                print(str(item))
+                arr.append(item)
+
+        new_arr = list(itertools.chain.from_iterable(arr))
+
+        for item in new_arr:
+            item[1].print()
+
+
+
