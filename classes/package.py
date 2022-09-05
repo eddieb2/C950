2b5a9780491b973787a06a5b152af78125f07541
Edward Blanciak <blanciak.edward2@gmail.com>
2022-09-05 18:34:51 -0400

    complete. update comments

--- classes/package.py
+++ classes/package.py
@@ -1,26 +1,32 @@
+from classes.hash_table import HashTable
+from classes.status_location import StatusLocation
 class Package:
-    def __init__(self, package_id, address, deadline, city, zip_code, weight):
+    def __init__(self, package_id, street, city, state, zip, deadline,
+                 weight, special_notes):
         self.id = package_id
-        self.address = address
-        self.deadline = deadline
+        self.street = street
         self.city = city
-        self.zip = zip_code
+        self.state = state
+        self.zip = zip
+        self.deadline = deadline
         self.weight = weight
-        self.tracking = {
-            ''' status = en-route, delivered, or at hub
-                {
-                "11:00am": {"location": "xyz", "status": "at hub"}
-                "12:00am": {"location": "xyz", "status": "en-route"},
-                "1:00pm": {"location": "xyz", "status": "delivered"}
-                }
-            '''
-        }
+        self.special_notes = special_notes
+        self.status = StatusLocation(0)
+
+        # Status
+        ''' status = en-route, delivered, or at hub
+        '''
+
 
     # Returns tracking for a particular time
-    def get_tracking(self):
-        return
+    def get_status(self):
+        return self.status.name
 
-    # Update current tracking
-    # Note: this will need to be updated for every minute in the day
-    def update_tracking(self):
-        return
+    # Update current status
+    def update_status(self, status):
+        self.status = StatusLocation(status)
+
+    # Print
+    def print(self):
+        print([self.id,self.street,self.state,self.zip,self.deadline,
+               self.weight,self.special_notes,self.status.name])
