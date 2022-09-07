0c8bd0d4815523bd9a5cabc1044ceffdfec290fd
Edward Blanciak <blanciak.edward2@gmail.com>
2022-09-07 13:50:55 -0400

    updates comments and reorganizes code

--- classes/hub.py
+++ classes/hub.py
@@ -9,27 +9,37 @@ class Hub:
         self.storage_size = 0
         self.distances = []
         self.addresses = []
-
         self.truck_1 = Truck(1)
         self.truck_2 = Truck(2)
         self.truck_3 = Truck(3)
-
         self.garage = [self.truck_1, self.truck_2, self.truck_3]
 
-    # Returns a package
-    def get_package(self, id):
-        return self.storage.lookup(str(id))
+    ####################
+    # Distance Methods #
+    ####################
 
     # Returns an 2-dimensional array of distances
     def get_distances(self):
         return self.distances
 
-    # Returns the distance between two locations
-    # takes strings (full address) and returns a float (distance)
+    # Returns the distance between two addresses
     def get_distance_between(self, full_street_address_1, full_street_address_2):
         location_1 = self._get_address_matrix_number(full_street_address_1)
         location_2 = self._get_address_matrix_number(full_street_address_2)
         return float(self.distances[location_1][location_2])
+
+    # Reads distance data from file and stores the data in the distance array
+    def add_distances(self, file_path):
+        # Read the distances file
+        with open(file_path) as distance_file:
+            distance_data = csv.reader(distance_file, delimiter=",", quotechar='"')
+            # Add the data to the distances array
+            for distance in distance_data:
+                self.distances.append(distance)
+
+    ###################
+    # Address Methods #
+    ###################
 
     # Returns the address number id (0-27) -- used for locating the address in the distance table
     def _get_address_matrix_number(self, full_street_address):
@@ -37,28 +47,31 @@ class Hub:
             if address[2] == full_street_address:
                 return int(address[0])
 
-
-    # Stores addresses
+    # Reads address data from a file and stores the data in the address array
     def add_addresses(self, file_path):
+        # Read the address data file
         with open(file_path) as address_file:
             address_data = csv.reader(address_file, delimiter=",", quotechar='"')
+            # Add data to the addresses array
             for address in address_data:
                 self.addresses.append(address)
 
-    # Stores distances
-    def add_distances(self, file_path):
-        with open(file_path) as distance_file:
-            distance_data = csv.reader(distance_file, delimiter=",", quotechar='"')
-            for distance in distance_data:
-                self.distances.append(distance)
 
-    # Adds packages to storage
+    ###################
+    # Package Methods #
+    ###################
+
+    # Returns a package from storage
+    def get_package(self, id):
+        return self.storage.lookup(str(id))
+
+    # Reads package data from a file, creates a Package, and adds it to storage.
     def add_packages(self, file_path):
-        # read the package data file
+        # Read the package data file
         with open(file_path) as package_file:
             package_data = csv.reader(package_file, delimiter=",", quotechar='"')
             for package in package_data:
-                # create a new Package
+                # Create a new Package
                 id = package[0]
                 street = package[1]
                 city = package[2]
@@ -74,6 +87,20 @@ class Hub:
                 self.storage.insert(id, new_package)
                 self.storage_size+=1
 
+    #########################
+    # Total Mileage Methods #
+    #########################
+
+    # Returns the total mileage for all trucks
+    def calculate_all_trucks_mileage(self):
+        total_mileage = 0
+        for truck in self.garage:
+            total_mileage+=truck.miles_traveled
+        return total_mileage
+
+    #################
+    # Print Methods #
+    #################
 
     # Prints all packages in storage
     def print_packages(self):
@@ -89,8 +116,3 @@ class Hub:
         for address in self.addresses:
             print(address)
 
-    def calculate_all_trucks_mileage(self):
-        total_mileage = 0
-        for truck in self.garage:
-            total_mileage+=truck.miles_traveled
-        return total_mileage
