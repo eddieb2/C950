b4003d211be529f7392265e569e45011572674bc
Edward Blanciak <blanciak.edward2@gmail.com>
2022-09-07 13:26:36 -0400

    comments updated. code reorganized.

--- classes/package.py
+++ classes/package.py
@@ -1,6 +1,4 @@
-from classes.hash_table import HashTable
 from classes.status_location import StatusLocation
-import datetime
 class Package:
     def __init__(self, package_id, street, city, state, zip, deadline,
                  weight, special_notes):
@@ -15,9 +13,9 @@ class Package:
         self.hub_departure_time = None
         self.time_delivered = None
 
-        # Status
-        ''' status = en-route, delivered, or at hub
-        '''
+    ##################
+    # Status Methods #
+    ##################
 
     # Returns a status for a particular time
     def calculate_status(self, time):
@@ -28,18 +26,25 @@ class Package:
         else:
             return StatusLocation(0).name
 
-    # Sets the time delivered for status purposes
+    ################
+    # Time Methods #
+    ################
+
+    # Sets the time that the package was delivered
     def set_time_delivered(self, time):
         self.time_delivered = time
 
+    # Sets the time that a package departed the hub
     def set_hub_departure_time(self, time):
         self.hub_departure_time = time
 
-    # Print
+    #################
+    # Print Methods #
+    #################
+
     def print(self):
         print([self.id,self.street,self.state,self.zip,self.deadline,
                self.weight,self.special_notes,self.hub_departure_time,self.time_delivered])
 
-
     def print_times(self):
-        print([self.hub_departure_time, self.time_left_hub])
+        print([self.hub_departure_time, self.hub_departure_time])
